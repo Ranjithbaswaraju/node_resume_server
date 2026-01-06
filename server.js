37270a4c90c6242e6be3b7750e2a9e3b401fad58
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2026-01-06 23:48:27 +0530

    updated

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express=require("express")
 const postResume=require("./routes/router")
 const app=express()
 const port=5100
+const cors=require("cors")
 app.use(express.json())
 app.use(express.urlencoded())
 
+
+app.use(cors({
+    origin:'http://localhost:5173/'
+}))
 
 app.use("/api/",postResume)
 
